92c469c5d5b34216fba3e9cfdebbf4cfc9cbfab1
GeorgHs <50588349+GeorgHs@users.noreply.github.com>
2020-05-20 10:59:23 +0200

    to arraylist

--- src/fowler/Customer.java
+++ src/fowler/Customer.java
@@ -5,34 +5,32 @@ import java.util.*;
 
 class Customer {
     private final String name;
-    private final Vector rentals = new Vector();
+    private final ArrayList<Rental> rentals = new ArrayList<>();
     public Customer (String newname){
         name = newname;
     }
     public void addRental(Rental arg) {
-        rentals.addElement(arg);
+    	rentals.add(arg);
     }
     public String getName (){
         return name;
     }
     public String statement() {
         double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
+        int frequentRenterPoints = 0; 
         StringBuilder result = new StringBuilder("fowler.Rental Record for " + this.getName() + "\n\tTitle\t\tDays\tAmount\n");
 
-        while (enum_rentals.hasMoreElements()) {
+        for (Rental rental : rentals) {
             double thisAmount = 0;
-            Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
-            thisAmount = amountFor(each);
+            thisAmount = amountFor(rental);
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) 
                 frequentRenterPoints ++;
             //show figures for this rental
-            result.append("\t").append(each.getMovie().getTitle()).append("\t\t").append(each.getDaysRented()).append("\t").append(thisAmount).append("\n");
+            result.append("\t").append(rental.getMovie().getTitle()).append("\t\t").append(rental.getDaysRented()).append("\t").append(thisAmount).append("\n");
             totalAmount += thisAmount;
         }
         //add footer lines
